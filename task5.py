716b7144ca0726c00be05d99702ffeffb418c359
sudnitshikov-ss <115349349+sudnitshikov-ss@users.noreply.github.com>
2022-12-25 20:32:39 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,8 +0,0 @@
-from random import sample
-
-def get_password(n=8) -> str:
-    hub = '0123456789ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    return "".join(sample(hub, n))
-
-
-print(get_password())
